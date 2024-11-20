c155c87378ce6560d0b418083b4216058566b341
Fluorurine <votruong830430073@gmail.com>
2024-11-20 19:44:12 +0700

    Hello 2

--- app/page.tsx
+++ app/page.tsx
@@ -9,28 +9,27 @@ import awsconfig from "./aws-exports";
 Amplify.configure(awsconfig, { ssr: true });
 
 export default function IndexPage() {
-	const formFields = {
-		signUp: {
-			username: {
-				order: 1,
-			},
-			email: {
-				order: 2,
-			},
-			password: {
-				order: 3,
-			},
-			confirm_password: {
-				order: 4,
-			},
-		},
-	};
-
+	// const formFields = {
+	// 	signUp: {
+	// 		username: {
+	// 			order: 1,
+	// 		},
+	// 		email: {
+	// 			order: 2,
+	// 		},
+	// 		password: {
+	// 			order: 3,
+	// 		},
+	// 		confirm_password: {
+	// 			order: 4,
+	// 		},
+	// 	},
+	// };
 	return (
 		<div className="min-h-screen flex flex-col justify-center items-center">
 			<header className="bg-white p-4 shadow-sm shadow-gray-300 w-full flex justify-between">
 				<h1 className="text-2xl pt-1 font-bold bg-gradient-to-r text-transparent from-indigo-500 to-orange-500 bg-clip-text">
-					Chatty 2 Truong
+					Chatty 3 Truong
 				</h1>
 				<div className="flex items-center">
 					<div className="mr-4 text-gray-600 hover:text-gray-80">
@@ -49,7 +48,7 @@ export default function IndexPage() {
 			</main>
 			<footer className="bg-white p-4 shadow-sm w-full text-xs">
 				<p className="text-center text-gray-600">
-					&copy; {new Date().getFullYear()} Your Company, all rights reserved.
+					{/* &copy; {new Date().getFullYear()} Your Company, all rights reserved. */}
 				</p>
 				<p className="text-center text-gray-600 text-xs">
 					AI answers should be verified before use.
